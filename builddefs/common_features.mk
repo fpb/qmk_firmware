231985cc99467e076d387259d95c5aca42c39ced
1Conan <me@1conan.com>
2022-10-23 22:53:31 +0800

    rename bt driver and fix build

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -885,7 +885,7 @@ ifeq ($(strip $(USBPD_ENABLE)), yes)
 endif
 
 BLUETOOTH_ENABLE ?= no
-VALID_BLUETOOTH_DRIVER_TYPES := bluefruit_le custom rn42
+VALID_BLUETOOTH_DRIVER_TYPES := bluefruit_le custom rn42 iton_bt
 ifeq ($(strip $(BLUETOOTH_ENABLE)), yes)
     ifeq ($(filter $(strip $(BLUETOOTH_DRIVER)),$(VALID_BLUETOOTH_DRIVER_TYPES)),)
         $(call CATASTROPHIC_ERROR,Invalid BLUETOOTH_DRIVER,BLUETOOTH_DRIVER="$(BLUETOOTH_DRIVER)" is not a valid Bluetooth driver type)
@@ -909,7 +909,7 @@ ifeq ($(strip $(BLUETOOTH_ENABLE)), yes)
         SRC += $(DRIVER_PATH)/bluetooth/rn42.c
     endif
 
-	ifeq ($(strip $(BLUETOOTH_DRIVER)), ITON_BT)
+	ifeq ($(strip $(BLUETOOTH_DRIVER)), iton_bt)
         OPT_DEFS += -DBLUETOOTH_ITON_BT
         SRC += $(DRIVER_PATH)/bluetooth/iton_bt.c
     endif
