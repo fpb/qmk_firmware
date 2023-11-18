facd0e97818c5f32329c6be853dfd81d667227fc
Fernando Birra <fpb@fct.unl.pt>
2023-11-18 19:45:52 +0000

    Updated license header and added missing one

--- keyboards/royal_kludge/rk61_rgb/keymaps/via/keymap.c
+++ keyboards/royal_kludge/rk61_rgb/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2023 Benjamín Ausensi
+/* Copyright (C) 2023 Fernando Birra
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
