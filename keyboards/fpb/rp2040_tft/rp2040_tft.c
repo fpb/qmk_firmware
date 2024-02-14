65610db043d3f3ea69ae2927badf7c722ec406bd
Fernando Birra <fpb@fct.unl.pt>
2024-02-14 17:03:17 +0000

    Setup for 2 displays

--- keyboards/fpb/rp2040_tft/rp2040_tft.c
+++ keyboards/fpb/rp2040_tft/rp2040_tft.c
@@ -7,24 +7,24 @@
 
 #include "graphics/qmklogo.qgf.h"
 
-static painter_device_t qp_display;
+static painter_device_t qp_display1;
 static painter_image_handle_t qp_image;
 
 void keyboard_post_init_kb(void) {
-    qp_display = qp_gc9107_make_spi_device(
-        PANEL_WIDTH, 
-        PANEL_HEIGHT, 
-        PANEL_CS, 
-        PANEL_DC, 
-        PANEL_RST, 
+    qp_display1 = qp_gc9107_make_spi_device(
+        PANEL1_WIDTH, 
+        PANEL1_HEIGHT, 
+        PANEL1_CS, 
+        PANEL1_DC, 
+        PANEL1_RST, 
         8, //spi_divisor, 
         0  //spi_mode
     );         // Create the display
-    qp_init(qp_display, QP_ROTATION_180);   // Initialise the display
-    qp_rect(qp_display, 0, 0, PANEL_WIDTH, PANEL_HEIGHT, 128, 255, 255, true);
+    qp_init(qp_display1, QP_ROTATION_180);   // Initialise the display
+    qp_rect(qp_display1, 0, 0, PANEL1_WIDTH, PANEL1_HEIGHT, 128, 255, 255, true);
     
     qp_image = qp_load_image_mem(gfx_qmklogo);
-    qp_drawimage(qp_display, 0, 0, qp_image);
+    qp_drawimage(qp_display1, 0, 0, qp_image);
 
     return keyboard_post_init_user();
 }
@@ -34,10 +34,10 @@ void housekeeping_task_kb(void) {
     if (timer_elapsed32(last_draw) > 33) { // Throttle to 30fps
         last_draw = timer_read32();
         // Draw 8px-wide rainbow down the left side of the display
-        for (int i = 0; i < PANEL_WIDTH; ++i) {
-            qp_line(qp_display, i, 0, i, 7, i, 255, 255);
+        for (int i = 0; i < PANEL1_WIDTH; ++i) {
+            qp_line(qp_display1, i, 0, i, 7, i, 255, 255);
         }
-        qp_flush(qp_display);
+        qp_flush(qp_display1);
     }
 }
 
--- keyboards/fpb/rp2040_tft/rp2040_tft.h
+++ keyboards/fpb/rp2040_tft/rp2040_tft.h
@@ -3,12 +3,22 @@
 
 #pragma once
 
-#define PANEL_WIDTH     128
-#define PANEL_HEIGHT    128
+#define PANEL1_WIDTH     128
+#define PANEL1_HEIGHT    128
 
-#define PANEL_DC        8
-#define PANEL_CS        9
-#define PANEL_CLK       10
-#define PANEL_MOSI      11
-#define PANEL_RST       12
-#define PANEL_BLK       13
+#define PANEL1_DC        27
+#define PANEL1_CS        17
+#define PANEL1_CLK       18
+#define PANEL1_MOSI      19
+#define PANEL1_RST       21
+#define PANEL1_BLK       13
+
+#define PANEL2_WIDTH     128
+#define PANEL2_HEIGHT    128
+
+#define PANEL2_DC        27
+#define PANEL2_CS        17
+#define PANEL2_CLK       18
+#define PANEL2_MOSI      19
+#define PANEL2_RST       22
+#define PANEL2_BLK       13
